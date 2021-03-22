05fca5ab7a5e29fdba40efdf3119c472657ea030
DenisZ <denis.zbitnev@geckodynamics.com>
2021-03-22 18:19:51 +0200

    add field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
             $table->string('name');
             $table->integer('age');
             $table->timestamps();
