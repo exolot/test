e040285a06bee002fa787eee87da7c172162ec2e
DenisZ <denis.zbitnev@geckodynamics.com>
2021-03-22 16:48:09 +0200

    add migrations(users, companies)

--- database/migrations/2021_03_22_144034_create_companies_table.php
+++ database/migrations/2021_03_22_144034_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->string('name');
-            $table->integer('age');
+            $table->date('started_at');
             $table->timestamps();
         });
     }
@@ -28,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 }
