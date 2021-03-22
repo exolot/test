1c94eed04139d7c6c0cf3fbe955e8664518e7b20
DenisZ <denis.zbitnev@geckodynamics.com>
2021-03-22 18:19:30 +0200

    add API call

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\{User, Company};
+use Illuminate\Http\JsonResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/populate', function () {
+    $json = file_get_contents(storage_path('challenge.json'));
+    $objs = json_decode($json, 1);
+
+    foreach ($objs['users'] as $user) {
+        $dbUser = User::create([
+            'user_id' => $user['id'],
+            'name' => $user['name'],
+            'age' => $user['age']
+        ]);
+        $dbUser->companies()->createMany(array_map(function ($item) {
+            return [
+                "company_id" => $item['id'],
+                "name" => $item['name'],
+                "started_at" => $item['started_at']
+            ];
+        }, $user['companies']));
+    }
+    return 'population done';
 });
+
+
+Route::get('/companies/{min?}/{max?}', function ($min = null, $max = null) {
+
+    $clause = Company::with('user');
+
+    if ($min && $max) {
+        return new JsonResponse($clause->whereHas('user', function ($query) use ($min, $max) {
+            return $query->where('age', '>', $min)->where('age', '<', $max);
+        })->get());
+    }
+
+    return new JsonResponse($clause->get());
+
+});
+
+
